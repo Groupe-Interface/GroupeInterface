5e724eb84dc2d9d17d7e18db9f487f9604cde76a
Mohamed-Aziz-Mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2021-06-19 19:08:06 +0100

    first commit

---
+++ src/Controller/PreInscriptionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PreInscriptionController extends AbstractController
+{
+    /**
+     * @Route("/pre/inscription", name="pre_inscription")
+     */
+    public function index(): Response
+    {
+        return $this->render('pre_inscription/index.html.twig', [
+            'controller_name' => 'PreInscriptionController',
+        ]);
+    }
+}
